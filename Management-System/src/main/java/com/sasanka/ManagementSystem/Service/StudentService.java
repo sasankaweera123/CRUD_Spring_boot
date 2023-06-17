4f3924f1835147eb4861871cef32bb6c5a427fd6
Sasanka <sajewsasanka1@gmail.com>
2023-06-17 18:14:13 +0530

    Create New Services

--- Management-System/src/main/java/com/sasanka/ManagementSystem/Service/StudentService.java
+++ Management-System/src/main/java/com/sasanka/ManagementSystem/Service/StudentService.java
@@ -1,6 +1,7 @@
 package com.sasanka.ManagementSystem.Service;
 
 import com.sasanka.ManagementSystem.Entity.Student;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.sasanka.ManagementSystem.Repository.StudentRepository;
 import org.springframework.stereotype.Service;
@@ -22,5 +23,26 @@ public class StudentService {
             return studentRepository.findAll();
     }
 
+    public void addNewStudent(Student student) {
+        if (student.getAge() < 0){
+            throw new IllegalStateException("Age cannot be negative");
+        }
+        studentRepository.save(student);
+    }
 
+    public void deleteStudent(Long studentId) {
+        boolean exists = studentRepository.existsById(studentId);
+        if (!exists){
+            throw new IllegalStateException("Student with id " + studentId + " does not exist");
+        }
+        studentRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId, String name) {
+        Student student = studentRepository.findById(studentId).orElseThrow(() -> new IllegalStateException("Student with id " + studentId + " does not exist"));
+        if (name != null && name.length() > 0 && !name.equals(student.getName())){
+            student.setName(name);
+        }
+    }
 }
