1883ae94f95abafe49a1aadac6b4af416719c10c
Sasanka <sajewsasanka1@gmail.com>
2023-06-28 10:47:00 +0530

    Update ManagementSystemApplicationTests.java
    Create basic cros Configure Function to test

--- Management-System/src/test/java/com/sasanka/ManagementSystem/ManagementSystemApplicationTests.java
+++ Management-System/src/test/java/com/sasanka/ManagementSystem/ManagementSystemApplicationTests.java
@@ -3,11 +3,17 @@ package com.sasanka.ManagementSystem;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ManagementSystemApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+	void corsConfigurer() {
+		ManagementSystemApplication msa = new ManagementSystemApplication();
+		msa.corsConfigurer();
+	}
+
 
 }
